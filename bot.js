6a144cccf81c9fc4bbcb61d343c21ac76ac57873
imomarksasa <41761073+imomarksasa@users.noreply.github.com>
2018-11-26 00:48:42 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+
+
+client.on('ready', () => {
+    client.user.setGame(`قوي جدا`,"http://twitch.tv/y04zgamer")
+    client.user.setStatus("dnd")
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+
+
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
