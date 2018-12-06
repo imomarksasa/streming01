cb9d5327c4de71a9b16d7e4e3627187c77bc0fcf
imomarksasa <41761073+imomarksasa@users.noreply.github.com>
2018-12-07 01:08:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,53 +14,6 @@ client.on('ready', () => {
 });
 
 
-bot.on('ready', () => {
-    console.log("Turning On Bot")
-    bot.user.setActivity("New Users", {type: 2});
-});
-
-bot.on('guildMemberAdd', member => {
-    let channel = member.guild.channels.find('name', 'public-chat');
-    let memberavatar = member.user.avatarURL
-        if (!channel) return;
-        let embed = new Discord.RichEmbed()
-        .setColor('RANDOM')
-        .setThumbnail(memberavatar)
-        .addField(':bust_in_silhouette: | name : ', `${member}`)
-        .addField(':microphone2: | Welcome!', `Welcome to the server, ${member}`)
-        .setFooter(`**${member.guild.name}**`)
-        .setTimestamp()
-
-        channel.sendEmbed(embed);
-});
-
-bot.on('guildMemberAdd', member => {
-
-    console.log(`${member}`, "has joined" + `${member.guild.name}`)
-
-});
-
-bot.on('guildMemberRemove', member => {
-    let channel = member.guild.channels.find('name', 'welcome-leave');
-    let memberavatar = member.user.avatarURL
-        if (!channel) return;
-        let embed = new Discord.RichEmbed()
-        .setColor('RANDOM')
-        .setThumbnail(memberavatar)
-        .addField('Name:', `${member}`)
-        .addField('Has Let the Server', ';(')
-        .addField('Bye Bye :(', 'We will all miss you!')
-        .addField('The server now as', `${member.guild.memberCount}` + " members")
-        .setFooter(`**${member.guild.name}`)
-        .setTimestamp()
-
-        channel.sendEmbed(embed);
-});
-
-bot.on('guildMemberRemove', member => {
-    console.log(`${member}` + "has left" + `${member.guild.name}` + "Sending leave message now")
-    console.log("Leave Message Sent")
-});
 
 
 // THIS  MUST  BE  THIS  WAY
