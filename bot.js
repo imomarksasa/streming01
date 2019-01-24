5a5240a97a5d2f316f6cdad651fc46ae2c8406c9
imomarksasa <41761073+imomarksasa@users.noreply.github.com>
2019-01-24 21:17:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,7 +31,7 @@ client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!devs.includes(message.author.id)) return;
       
-  if (message.content.startsWith('(prefix))ply')) {
+  if (message.content('ply')) {
     client.user.setGame(argresult);
       message.channel.send(`**✅   ${argresult}**`)
   } else 
