898d583fe7336da8aa9d687f1c8689bff5c33be7
imomarksasa <41761073+imomarksasa@users.noreply.github.com>
2019-01-24 21:42:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,7 +25,7 @@ client.user.setStatus("dnd");
 
 
 
-const devs = ["321476121956974595"];
+const devs = ["323224691911163914"];
 const adminprefix = ["-"];
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
